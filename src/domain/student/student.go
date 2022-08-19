261ea586da6c8fa9bfe6eed8eccd1fb6063680f4
Jean <jean.carlosmolossi@gmail.com>
2022-08-18 22:27:09 -0300

    feat(student): added domain usecases

--- src/domain/student/student.go
+++ src/domain/student/student.go
@@ -8,6 +8,21 @@ type Student interface {
 	baseuser.BaseUser
 }
 
+// Usecases is the student usecases
+
+// GetStudents is the usecase to get all students
+// Not implemented yet
 type GetStudents func() ([]Student, error)
 
+// RegisterStudent is the usecase to register a new student
 type RegisterStudent func(Student) error
+
+// GetMe is the usecase to get the current student
+// It will use the session token
+type GetMe func(GetMeOptions) (Student, error)
+
+// GetMeOptions is the options for the GetMe usecase
+type GetMeOptions struct {
+	StudentID string   `json:"student_id" example:"1"`
+	Fields    []string `query:"fields" example:"name,email"`
+}
