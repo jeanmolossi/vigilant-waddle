efd6109f865f2a689f1ee6c357015e9c67087a50
Jean <jean.carlosmolossi@gmail.com>
2022-08-18 22:22:40 -0300

    docs(swagger): improve register student docs

--- src/core/modules/student/handler/register_student.go
+++ src/core/modules/student/handler/register_student.go
@@ -19,10 +19,10 @@ import (
 // @ID register-student
 // @Produce json
 // @Param student body adapter.RegisterStudent true "Student"
-// @Success 201 {object} student.Student
-// @Failure 400 {object} http_error.HTTPBadRequestError
-// @Failure 409 {object} http_error.HTTPError
-// @Failure 500 {object} http_error.HTTPError
+// @Success 201 {object} HttpNewStudent
+// @Failure 400 {object} http_error.HTTPBadRequestError "Bad request"
+// @Failure 409 {object} http_error.HTTPError "User with that email already exists"
+// @Failure 500 {object} http_error.HTTPError "An error occurred"
 // @Router /student [post]
 func RegisterStudent() echo.HandlerFunc {
 	db := database.GetConnection()
@@ -50,6 +50,26 @@ func RegisterStudent() echo.HandlerFunc {
 			return http_error.Handle(c, err)
 		}
 
-		return c.JSON(http.StatusCreated, s)
+		return c.JSON(http.StatusCreated, NewHttpNewStudent(s))
+	}
+}
+
+// Http responses
+
+// HttpStudent is a student representation for http response
+type HttpStudent struct {
+	ID    string `json:"id" example:"550e8400-e29b-41d4-a716-446655440000" format:"uuid"`
+	Email string `json:"email" example:"john@doe.com" format:"email"`
+}
+
+// HttpNewStudent is a student representation for http response
+type HttpNewStudent struct {
+	Data HttpStudent `json:"data"`
+}
+
+// NewHttpNewStudent creates a new HttpNewStudent
+func NewHttpNewStudent(s student.Student) *HttpNewStudent {
+	return &HttpNewStudent{
+		Data: HttpStudent{s.GetID(), s.GetEmail()},
 	}
 }
