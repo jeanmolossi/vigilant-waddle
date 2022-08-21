86850414fcf0cd78ca0f8ce6bd22741d2c6ef945
Jean <jean.carlosmolossi@gmail.com>
2022-08-21 16:30:42 -0300

    feat(student_errors): added student errors

--- src/domain/student/errors.go
+++ src/domain/student/errors.go
@@ -6,6 +6,8 @@ var ErrInvalidStudentProp = errors.New("invalid student property")
 var ErrNoDataToSync = errors.New("no data to sync")
 var ErrEmptyPropertyValue = errors.New("empty property value")
 var ErrEmailAlreadyExists = errors.New("email already exists")
+var ErrNoStudentsFound = errors.New("no students found")
+var ErrInvalidCredentials = errors.New("invalid credentials")
 
 // GetMeOptions validations
 var ErrMissingStudentID = errors.New("missing student id")
