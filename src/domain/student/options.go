01a5ea0329a528e5a25c4034242e9db048c318b6
Jean <jean.carlosmolossi@gmail.com>
2022-08-21 16:31:43 -0300

    refactor(student_options): removed unnecessary cast type

--- src/domain/student/options.go
+++ src/domain/student/options.go
@@ -1,13 +1,15 @@
 package student
 
-import baseuser "github.com/jeanmolossi/vigilant-waddle/src/domain/base_user"
+import (
+	baseuser "github.com/jeanmolossi/vigilant-waddle/src/domain/base_user"
+)
 
 // WithID returns a baseuser.Option that sets the ID of a student.
 //
 // It receives a string and sets the ID of the student.
 // If the string is empty, it will return an error.
 func WithID(id string) baseuser.Option {
-	return baseuser.Option(func(u baseuser.BaseUser) error {
+	return func(u baseuser.BaseUser) error {
 		if id == "" {
 			return ErrEmptyPropertyValue
 		}
@@ -18,7 +20,7 @@ func WithID(id string) baseuser.Option {
 		}
 
 		return baseuser.ErrInvalidBaseUsrImplementation
-	})
+	}
 }
 
 // WithEmail returns a baseuser.Option that sets the Email of a student.
@@ -26,26 +28,28 @@ func WithID(id string) baseuser.Option {
 // It receives a string and sets the Email of the student.
 // If the string is empty, it will return an error.
 func WithEmail(email string) baseuser.Option {
-	return baseuser.Option(func(u baseuser.BaseUser) error {
+	return func(u baseuser.BaseUser) error {
 		if email == "" {
 			return ErrEmptyPropertyValue
 		}
 
 		if usr, ok := u.(*student); ok {
 			usr.email = email
+			return nil
 		}
 
 		return baseuser.ErrInvalidBaseUsrImplementation
-	})
+	}
 }
 
 // WithPassword returns a baseuser.Option that sets the Password of a student.
 func WithPassword(password string) baseuser.Option {
-	return baseuser.Option(func(u baseuser.BaseUser) error {
+	return func(u baseuser.BaseUser) error {
 		if usr, ok := u.(*student); ok {
 			usr.password = password
+			return nil
 		}
 
 		return baseuser.ErrInvalidBaseUsrImplementation
-	})
+	}
 }
